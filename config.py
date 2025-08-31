6affd5834f3991601f7e780c61d6a5500a069d21
David <davidadakole230@gmail.com>
2025-08-31 13:06:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @BotFather
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # your Render app URL, e.g. https://solinvest.onrender.com
+BOT_TOKEN = os.getenv("8244668562:AAENIRPN_Luh-3IGXP5v9o25Zx8-sUbkqxw","")
